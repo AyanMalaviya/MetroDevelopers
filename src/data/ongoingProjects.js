dd907c811a2609f9f6d21f1b66cc8bbeb4fb79ab
Smit <smitkazani111@gmail.com>
2025-12-25 12:53:07 +0530

    Added data config files for project
    to add, edit, remove projects indiviually from respective projects page

---
+++ src/data/ongoingProjects.js
@@ -0,0 +1,16 @@
+// src/data/ongoingProjects.js
+
+export const ongoingProjects = [
+  {
+    name: 'Metro Logistics Hub - Phase 2',
+    image: '/src/assets/images/ongoing/metro-logistics-phase2.jpg',
+    description:
+      'Grade-A warehousing and logistics hub with expandable industrial sheds and excellent highway connectivity.',
+  },
+  {
+    name: 'Automotive Components Cluster',
+    image: '/src/assets/images/ongoing/auto-components-cluster.jpg',
+    description:
+      'Specialized sheds designed for auto component manufacturers with high floor load capacity and ample docking bays.',
+  },
+];
---
+++ src/data/pastProjects.js
@@ -0,0 +1,16 @@
+// src/data/pastProjects.js
+
+export const pastProjects = [
+  {
+    name: 'Metro Warehousing Complex',
+    image: '/src/assets/images/past/metro-warehousing-complex.jpg',
+    description:
+      '1,00,000 sq.ft of fully leased warehousing space with modular units for 10+ clients.',
+  },
+  {
+    name: 'Cold Storage & FMCG Park',
+    image: '/src/assets/images/past/cold-storage-fmcg-park.jpg',
+    description:
+      'Temperature-controlled storage and distribution hub serving multiple FMCG brands.',
+  },
+];
