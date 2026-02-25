11eceea99eb286bd3e2978f3ee7e8c23d36cb3e8
AyanMalaviya <ayanmalaviya111@gmail.com>
2026-02-25 15:40:47 +0530

    Logo fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -249,8 +249,8 @@ const HomePage = () => {
                 src="/MDLogoBG.png"
                 alt="Metro Enterprise - Industrial Shed Developer Ahmedabad"
                 width={288} height={288}
-                initial={{ opacity: 0.2, scale: 4, x:10 , y: 180 }}
-                animate={{ opacity: 1, scale: 1.2, x: 10, y: 40 }}
+                initial={{ opacity: 0.2, scale: 2, y: 180 }}
+                animate={{ opacity: 1, scale: 1, y: 40 }}
                 transition={{ duration: 2, ease: 'easeOut' }}
                 className="w-36 sm:w-52 md:w-64 lg:w-72 drop-shadow-[0_6px_30px_rgba(0,0,0,0.75)] select-none"
               />
